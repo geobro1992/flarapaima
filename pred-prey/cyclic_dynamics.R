92d4a826384abc134c4c2c2a42fc28aa9096cdb3
geobro1992 <30474759+geobro1992@users.noreply.github.com>
2023-03-21 21:20:25 -0400

    Add files via upload

--- pred-prey/cyclic_dynamics.R
+++ pred-prey/cyclic_dynamics.R
@@ -7,14 +7,21 @@
 
 ###################
 
+# number of years
+n.years = 200
+
+# scenarios
+F.sens = data.frame(scalar = rep(c(0, 0.5, 1, 2, 3, 4),2), 
+                    lag = c(1,1,1,1,1,1,2,2,2,2,2,2)) # sensitivity to changes in fish abundance
+
 # Define ecological state variables
-N <- numeric(200) # Population size
-r <- numeric(200) # Intrinsic growth rate
+N <- matrix(nrow = n.years, ncol = length(F.sens[,1])) # Population size
+r <- matrix(nrow = n.years, ncol = length(F.sens[,1])) # Intrinsic growth rate
 
 # Define social state variables
-F <- numeric(200) # Fishing effort
-C <- numeric(200) # Catch rate
-P <- numeric(200) # Policy regulations
+F <- matrix(nrow = n.years, ncol = length(F.sens[,1])) # Fishing effort
+C <- matrix(nrow = n.years, ncol = length(F.sens[,1])) # Catch rate
+P <- matrix(nrow = n.years, ncol = length(F.sens[,1])) # Policy regulations
 
 # Define model parameters
 alpha <- 0.05 # Growth rate coefficient
@@ -23,22 +30,71 @@ gamma <- 0.02 # Catch rate coefficient
 delta <- 0.1 # Policy coefficient
 
 # Define initial values for year 1
-N[1] <- 300
-F[1] <- 0.2
+N[1:2,] <- 300
+F[1:2,] <- 0.2
 
 # Define model equations
-for (t in 2:200) {
-  # Ecological dynamics
-  r[t] <- alpha * N[t-1]
-  N[t] <- N[t-1] + r[t] * (1 - N[t-1]/K) - C[t-1] 
-  
-  # Social dynamics
-  C[t] <- gamma * F[t-1] * N[t-1]     # catch is a function of population size, fishing intensity, and catch rate (gamma)
-  P[t] <- (N[t-1]/K) - 0.5            # policy enforcement becomes more lenient when population approaches carrying capacity 
-  F[t] <- F[t-1] + (P[t-1] * F[t-1])  # fishing pressure changes with yearly changes in enforcement
+for(j in 1:length(F.sens[,1])){
+  for (t in 3:n.years) {
+
+        # Social dynamics
+    C[t-1,j] <- gamma * F[t-1,j] * N[t-1,j]             # catch is a function of population size, fishing intensity, and catch rate (gamma)
+    P[t-1,j] <- ((N[t-F.sens[j,2],j]/K) -  0.5) * F.sens[j,1]  # policy enforcement becomes more lenient when population approaches carrying capacity (can include lagged policy changes) 
+    F[t,j] <- F[t-1,j] + (P[t-1,j] * F[t-1,j])          # fishing pressure changes with yearly changes in enforcement
+    
+    # Ecological dynamics
+    r[t-1,j] <- alpha * N[t-1,j]
+    N[t,j] <- N[t-1,j] + r[t-1,j] * (1 - N[t-1,j]/K) - C[t-1,j] 
+    
+    
+  }
 }
 
+
 # Plot population size and fishing effort over time
-par(mfrow = c(2,1), mar = c(2,5,2,2))
-plot(N, type = "l", ylab = "Population size")
-plot(C, type = "l", ylab = "Catch / Fishing Effort", col = "red")
+pdf(file = "cyclic_lag.pdf", width = 6, height = 8)
+
+par(mar=c(0,0,0,0))
+
+layout(matrix(c(1,2,3,4,5,1,6,7,8,9),ncol=2),heights=c(1,3,3,3,3))
+
+plot.new()
+
+text(0.25,0.2,"Lag = 1 year",cex=2,font=2)
+text(0.8,0.2,"Lag = 2 years",cex=2,font=2)
+
+par(mar=c(2.5,2.5,1,1))
+plot(N[,1], type = "l", ylab = "Population size", ylim = c(0,max(N, na.rm=T)+50))
+plot(N[,3], type = "l", ylab = "Population size", ylim = c(0,max(N, na.rm=T)+50))
+plot(N[,5], type = "l", ylab = "Population size", ylim = c(0,max(N, na.rm=T)+50))
+plot(N[,6], type = "l", ylab = "Population size", ylim = c(0,max(N, na.rm=T)+50))
+plot(N[,7], type = "l", ylab = "Population size", ylim = c(0,max(N, na.rm=T)+50))
+plot(N[,9], type = "l", ylab = "Population size", ylim = c(0,max(N, na.rm=T)+50))
+plot(N[,11], type = "l", ylab = "Population size", ylim = c(0,max(N, na.rm=T)+50))
+plot(N[,12], type = "l", ylab = "Population size", ylim = c(0,max(N, na.rm=T)+50))
+
+dev.off()
+
+# Plot population size and fishing effort over time
+pdf(file = "cyclic_lag_catch.pdf", width = 6, height = 8)
+
+par(mar=c(0,0,0,0))
+
+layout(matrix(c(1,2,3,4,5,1,6,7,8,9),ncol=2),heights=c(1,3,3,3,3))
+
+plot.new()
+
+text(0.25,0.2,"Lag = 1 year",cex=2,font=2)
+text(0.8,0.2,"Lag = 2 years",cex=2,font=2)
+
+par(mar=c(2.5,2.5,1,1))
+plot(C[,1], type = "l", ylab = "Catch")
+plot(C[,3], type = "l", ylab = "Catch")
+plot(C[,5], type = "l", ylab = "Catch")
+plot(C[,6], type = "l", ylab = "Catch")
+plot(C[,7], type = "l", ylab = "Catch")
+plot(C[,9], type = "l", ylab = "Catch")
+plot(C[,11], type = "l", ylab = "Catch")
+plot(C[,12], type = "l", ylab = "Catch")
+
+dev.off()
